36ed1bf40a89ae8f6bca9de19ee9fdadd60d78a2
Tom <santek.vg@gmail.com>
2016-11-28 14:49:56 +0100

    update links and scroll

--- js/main.js
+++ js/main.js
@@ -49,22 +49,20 @@ $(document).ready(function(){
 
 
 /* smood scroll */
-
-    $(document).ready(function(){
-
-      $("a").on('click', function(event) { 
-        if (this.hash !== "") {     
-          event.preventDefault();  
-          var hash = this.hash;
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
           $('html, body').animate({
-            scrollTop: $(hash).offset().top
-          }, 800, function(){
-            window.location.hash = hash;
-          });
-        } 
-      });
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
     });
-
+  });
 
 
 /* about us */
